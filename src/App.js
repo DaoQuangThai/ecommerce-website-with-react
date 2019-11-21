477b95da572daae47f2b80362dd3af1a4a0175e9
daoquangthai <quangthai999999@gmail.com>
2019-11-21 21:19:16 +0700

    add header link to shopage and contact page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ShopPage from "./Pages/shop/ShopPage";
+import Header from "./Components/Header/Header";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
